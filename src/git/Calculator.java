473f5f77c853a77a0f433c05e0a135d4f434af0e
iam-zohaib <114738880+iam-zohaib@users.noreply.github.com>
2023-10-07 21:28:10 +0500

    file uploaded

---
+++ src/git/Calculator.java
@@ -0,0 +1,14 @@
+package git;
+
+public class Calculator {
+int add(int num1,int num2) {
+	return num1+num2;
+}
+int subtract(int num1,int num2) {
+	return num1-num2;
+}
+	public Calculator() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
